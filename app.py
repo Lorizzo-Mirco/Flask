587494d9671031165e918a3a4aa275a186a718e2
Mirco Lorizzo <45140218+Lorizzo-Mirco@users.noreply.github.com>
2025-04-16 11:57:10 +0000

    Rimuovi file non necessari e aggiungi funzionalità per l'aggiunta di elementi nella lista

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 lista = ['pippo', 'pluto', 'paperino', 'paperone', 'minnie', 'topolino']
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        elemento = request.form.get('elemento')
+        lista.append(elemento)
+        render_template("index.html", lista=lista)
+        
+
     return render_template('index.html', lista=lista)
 
 @app.route('/profilo')
 def profilo():
     return "Questa è la pagina del profilo!"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
